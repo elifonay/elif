8e7d3ee64e21910ee22165ec78d6917c67c6d357
elifonay <163148633+elifonay@users.noreply.github.com>
2024-06-04 18:37:39 +0300

    d

--- script.js
+++ script.js
@@ -66,3 +66,21 @@ function countRabbits() {
         alert("Rabbit number " + i);
     }
     }
+
+
+    const menu = document.getElementById('menu');
+
+    const circle = document.getElementById('circle');
+    
+
+
+
+
+document.addEventListener('mousemove', (e) => {
+    const height = circle.offsetHeight;
+    const width = circle.offsetWidth;
+
+    circle.style.left = `${e.pageX - width/2}px`;
+    circle.style.top=` ${e.pageY - height/2}px`;
+
+ });
